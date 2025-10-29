462e015ed5cac5070a57f55d387783c692c67ecc
Riyansh Ritesh Srivastava <riyanshsrivastava.1k21@gmail.com>
2025-10-29 16:56:55 -0500

    Testing Commiting Through IntelliJ IDEAL

--- src/main/java/net/mdarkhead/halloweenhackathon2025/MdhzHalloweenHackathon2025.java
+++ src/main/java/net/mdarkhead/halloweenhackathon2025/MdhzHalloweenHackathon2025.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//
 public class MdhzHalloweenHackathon2025 implements ModInitializer {
 	public static final String MOD_ID = "mdhz-halloween-hackathon-2025";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
